0f03e55e2cca5a35cec031105fdd9ed0cfa1bb88
matheusquintaes <matheus.quintaes@gmail.com>
2018-01-19 10:11:59 -0200

    creating application state

--- src/app.js
+++ src/app.js
@@ -1,31 +1,16 @@
 'use strict'
 
-import React from 'react'
-import Search from './components/search'
-import UserInfo from './components/user-info'
-import Actions from './components/actions'
-import Repos from './components/repos'
+import React, { Component } from 'react'
+import AppContent from './components/app-content'
 
-const App = () => (
-  <div className='app'>
-    <Search />
-    <UserInfo />
-    <Actions />
-    <Repos
-      className='repos'
-      title='Repositórios'
-      repos={[{
-        name: 'Nome do Repositório',
-        link: '#'
-      }]} />
-    <Repos
-      className='starred'
-      title='Favoritos'
-      repos={[{
-        name: 'Nome do Repositório',
-        link: '#'
-      }]} />
-  </div>
-)
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {}
+  }
 
+  render () {
+    return <AppContent />
+  }
+}
 export default App
---
+++ src/components/app-content.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import React from 'react'
+import Search from './search'
+import UserInfo from './user-info'
+import Actions from './actions'
+import Repos from './repos'
+
+const AppContent = () => (
+  <div className='app'>
+    <Search />
+    <UserInfo />
+    <Actions />
+    <Repos
+      className='repos'
+      title='Repositórios'
+      repos={[{
+        name: 'Nome do Repositório',
+        link: '#'
+      }]} />
+    <Repos
+      className='starred'
+      title='Favoritos'
+      repos={[{
+        name: 'Nome do Repositório',
+        link: '#'
+      }]} />
+  </div>
+)
+
+export default AppContent
